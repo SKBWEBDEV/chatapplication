765a6cbc052c2627a3e3be19b3d2a7d743b65679
SKBWEBDEV <najmujshakib025@gmail.com>
2025-10-11 11:55:40 +0600

    solve

--- src/components/Chatapplication/Ragister.jsx
+++ src/components/Chatapplication/Ragister.jsx
@@ -60,7 +60,7 @@ const Ragister = () => {
       setPasswordError("at least one lowercase,one uppercase letter,one digit,one special characte,at least 8 characters long")
     }
   }
- }
+ }  
 
 
 
@@ -70,56 +70,57 @@ const Ragister = () => {
   
   return (
     <div>
-      <div className="md:flex md:items-center ml-4 md:ml-0">
-        <div className="md:w-[50%] flex justify-end ">
+      <div className="md:flex md:items-center ">
+        <div className="md:w-[50%] flex md:justify-end justify-center">
           <div className="md:mr-[70px]">
-            <h1 className="font-bold text-[34px] text-[#11175D] font-secondary">Get started with easily register</h1>
-          <p className="text-[#808080] font-secondary text-[20px] font-normal">Free register and you can enjoy it</p>
+            <h1 className="font-bold md:text-[34px] text-[23px] text-[#11175D] font-secondary">Get started with easily register</h1>
+          <p className="text-[#808080] font-secondary md:text-[20px] font-normal">Free register and you can enjoy it</p>
 
 
           <div className="relative mt-[40px]">
             <p className="text-[#585D8E] text-[13px] font-secondary font-semibold absolute top-[-8px] left-[55px] bg-white w-35 text-center tracking-[2px]">Email Address</p>
-            <input className=" py-[15px] pl-[65px] outline-0 pr-[66px] w-[368px] font-secondary text-[20px] font-semibold 
+            <input className=" py-[15px] pl-[65px] outline-0 md:pr-[66px] md:w-[368px] font-secondary text-[20px] font-semibold 
             border-[#B8B8CE] rounded-[8px] border-3 text-[#11175D]" type="email"
             onChange={handleEmail}
             value={email}
             placeholder="Ladushing691@gmail.com "/>
 
-            <p className="mt-3 bg-gray-600 text-white font-bold text-center w-[368px] px-5 rounded-[8px]">{errorEmail}</p>
+            <p className="mt-3 bg-gray-600 text-white font-bold text-center md:w-[368px] w-80 rounded-[8px]">{errorEmail}</p>
             
           </div>
 
            <div className="relative mt-[40px]">
-            <input className=" py-[15px] pl-[65px] pr-[66px] outline-0 w-[368px] font-secondary text-[20px] font-semibold 
+            <input className=" py-[15px] pl-[65px]  outline-0 md:pr-[66px] md:w-[368px] font-secondary text-[20px] font-semibold 
             border-[#B8B8CE] rounded-[8px] border-3 text-[#11175D]" type="text"
             onChange={nameUpdate}
             value={name}
             placeholder="Ladushing GTG "/>
             <p className="text-[#585D8E] text-[13px] font-secondary font-semibold absolute top-[-8px] left-[45px] bg-white w-30 text-center tracking-[2px]">Ful name</p>
-              <p className="mt-3 bg-gray-600 text-white font-bold text-center w-[368px] px-5 rounded-[8px]">{errorName}</p>
+              <p className="mt-3 bg-gray-600 text-white font-bold text-center md:w-[368px] w-80 rounded-[8px]">{errorName}</p>
 
           </div>
 
            <div className="relative mt-[40px]">
-            <input className=" py-[15px] pl-[65px] outline-0 pr-[66px] w-[368px] font-secondary text-[20px] font-semibold 
+            <input className=" py-[15px] pl-[65px] outline-0 md:pr-[66px] md:w-[368px] font-secondary text-[20px] font-semibold 
             border-[#B8B8CE] rounded-[8px] border-3 text-[#11175D]" type="text"
             onChange={passwordUpdate}
             placeholder="Password "/>
             <p className="text-[#585D8E] text-[13px] font-secondary font-semibold absolute top-[-8px] left-[45px] bg-white w-30 text-center tracking-[2px]">Password</p>
 
-            <p className="mt-3 bg-gray-600 text-white font-bold text-center w-[368px] px-5 rounded-[8px]">{passwordError}</p>
+            <p className="mt-3 bg-gray-600 text-white font-bold text-center md:w-[368px] w-80 rounded-[8px]">{passwordError}</p>
           </div>
            
            <div className="mt-[40px] ">
             <button
-            onClick={signUp} className="text-[20px] outline-0 w-[368px] bg-[#1E1E1E] rounded-full text-white py-[20px]  font-semibold font-secondary cursor-pointer"><a href="#">Sign up</a></button>
+            onClick={signUp} className="text-[20px]  outline-0 w-50 md:w-[368px] bg-[#1E1E1E] rounded-full text-white 
+            py-[20px]  font-semibold font-secondary cursor-pointer"><a href="#">Sign up</a></button>
             <p className="mt-[30px] text-[#03014C] ml-22 text-[13px] font-primary font-normal">Already  have an account ? <span className="text-[#EA6C00]">Sign I n</span></p>
            </div>
 
           
           </div>
         </div>
-        <div className="md:w-[50%] mr-4">
+        <div className="md:w-[50%] mr-4 md:mr-0">
           <img className="h-screen w-full object-cover" src={Reg} alt="" />
         </div>
       </div>
