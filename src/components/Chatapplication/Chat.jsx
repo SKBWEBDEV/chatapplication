3ef21c1d6b6701ad212150a5113747e546176f82
SKBWEBDEV <najmujshakib025@gmail.com>
2025-10-21 09:23:09 +0600

    hover add

--- src/components/Chatapplication/Chat.jsx
+++ src/components/Chatapplication/Chat.jsx
@@ -6,8 +6,9 @@ const Chat = () => {
       <div className="flex justify-center mt-40 md:mt-65">
         <Link to = "/register"><img src={Chatapp} alt="" /></Link>
       </div>
-      <div className="flex justify-center mt-2 font-bold text-[25px] text-[#FF4500]">
-        <Link to = "/register"><p>Chat App</p></Link>
+      <div className="flex justify-center mt-2 font-bold text-[25px] text-black">
+        <button className="border-none py-2.5 px-10 rounded-lg shadow-[-3px_8px_24px_4px_rgba(0,0,0,0.64)] hover:bg-black hover:text-white transition duration-500 ">
+          <Link to = "/register"><p>Get Started</p></Link></button>
       </div>
     </div>
   )
